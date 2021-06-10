11bcb3ef40ad16e0b9cab339ae1615d80930cb6b
Felipe Rafael Wanner <felipe.w.pakua@gmail.com>
2021-06-10 15:29:13 -0300

    consertando barra pesquisa responsiva

--- assets/js/barra-pesquisa-mobile.js
+++ assets/js/barra-pesquisa-mobile.js
@@ -1,17 +1,22 @@
-const $lupa = document.querySelector(".lupa-js");
-const $barraDePesquisa = document.querySelector(".barra-pesquisa-js");
+const $lupa = document.querySelector(".lupa-js")
+const $barraDePesquisa = document.querySelector(".barra-pesquisa-js")
+const $logoAlura = document.querySelector(".logo-alura-dev")
 let mostra = true;
 
 $lupa.addEventListener("click", function(){
   if(mostra){
-    $lupa.classList.remove("lupa");
-    $lupa.classList.add("lupaX");
-    $barraDePesquisa.classList.remove("barra-pesquisa__invisivel");
+    $lupa.classList.remove("lupa")
+    $lupa.classList.add("lupaX")
+    $barraDePesquisa.classList.add("barra-pesquisa__largura-total")
+    $barraDePesquisa.classList.remove("barra-pesquisa__invisivel")
+    $logoAlura.classList.add("invisivel")
     mostra = false;
   } else if (!mostra){
-    $lupa.classList.add("lupa");
-    $lupa.classList.remove("lupaX");
-    $barraDePesquisa.classList.add("barra-pesquisa__invisivel");
-    mostra = true;
+    $lupa.classList.add("lupa")
+    $lupa.classList.remove("lupaX")
+    $barraDePesquisa.classList.add("barra-pesquisa__invisivel")
+    $barraDePesquisa.classList.remove("barra-pesquisa__largura-total")
+    $logoAlura.classList.remove("invisivel")
+    mostra = true
   }
-});
+})
